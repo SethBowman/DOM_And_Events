18dc039576c0a4605ede1a3f0db7cfac6acad3ac
Seth Bowman <brandonsethbowman@gmail.com>
2021-06-25 14:29:37 -0500

    Started practice

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+let main = document.getElementsByTagName("main");
+
+let headings = document.getElementsByClassName("heading");
+
+let paras = document.getElementsByClassName("content");
+
+let a = document.getElementById("navigation");
+
+let firstChild = document.getElementById("firstSection");
+
+console.log(main);
+console.log(headings);
+console.log(paras);
+console.log(a);
+console.log(main[0]);
+
+paras[0].textContent = "I just changed you";
+paras[0].style.color = "red";
+main[0].id = "homeContent";
+
+let newElement = document.createElement("section");
+
+// main[0].appendChild(newElement);
+main[0].insertBefore(newElement, firstChild);
+
+// removeChild();
+
+// replaceChild(newNode, oldNode);
+
+let button = document.createElement("button");
+button.textContent = "Sign Up";
+
+firstChild.replaceChild(button, paras[0]);
+// firstChild.removeChild(a); // removes a tag
